f7126dd66f512bafe8847e303680fe24fc157f54
Jaime Lara Carrillo <G00425663@atu.ie>
2023-04-04 11:41:04 +0100

    Weekly task 6

---
+++ squareroot.py
@@ -0,0 +1,16 @@
+# This program takes a positive floating-point number as input and outputs an approximation of its square root using the Newton Method
+# Author: Jaime Lara Carrillo
+
+num = float(input("Please insert a number: "))
+def sqrt(num):
+    approx = num / 2  # We assume an initial approximation of half the number
+    while True:
+        diff = abs(approx - (num / approx))
+        if diff < 0.0001:
+            break
+        approx = (approx + (num / approx)) / 2
+    return approx
+
+result = sqrt(num)
+rounded_result = round(result, 2)
+print(f"The square root of {num} is approx. {rounded_result}")
