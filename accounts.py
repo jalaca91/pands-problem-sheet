dbc209ba879643c09238bc43d65e1254aee435d7
Jaime Lara Carrillo <110190460+jalaca91@users.noreply.github.com>
2023-04-05 14:06:54 +0100

    Delete accounts.py

--- accounts.py
+++
@@ -1,7 +0,0 @@
-# Reads in a 10 character account number and outputs the account number
-# with only the last 4 digits showing (and the first 6 digits replaced with Xs)
-# Author: Jaime Lara Carrillo 
-
-account_number = input("Enter account number: ")
-final_account = account_number.replace(account_number[:-4], "X" * 6)
-print(final_account)
