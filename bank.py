d4253c4f14ec9ac9ed57a07f47e12c7f07e7e6c1
Jaime Lara Carrillo <110190460+jalaca91@users.noreply.github.com>
2023-04-05 14:12:51 +0100

    Delete bank.py

--- bank.py
+++
@@ -1,9 +0,0 @@
-# Bank.py
-# Returns the sum of 2 amounts in cent to euro
-# Author: Jaime Lara
-
-amount1 = int(input("Enter amount1 (in cent): "))
-amount2 = int(input("Enter amount2 (in cent): "))
-total = (amount1 + amount2)
-cent_to_euro_conversion = total/100
-print(total, "cent are", "€", cent_to_euro_conversion)
