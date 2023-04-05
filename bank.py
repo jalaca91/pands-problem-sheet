f93bf6cc4f735f33a768029aaaa717051f998ef4
Jaime Lara Carrillo <G00425663@atu.ie>
2023-04-05 14:16:16 +0100

    Weekly task 2

---
+++ bank.py
@@ -0,0 +1,8 @@
+# Returns the sum of 2 amounts in cent to euro
+# Author: Jaime Lara
+
+amount1 = int(input("Enter amount1 (in cent): "))
+amount2 = int(input("Enter amount2 (in cent): "))
+total = (amount1 + amount2)
+cent_to_euro_conversion = total/100
+print(f"{total} cents are €{cent_to_euro_conversion}")
