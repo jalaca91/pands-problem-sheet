8d5fe3c1b598b0ec2f447bfdeec5b61d074e9c96
Jaime Lara Carrillo <G00425663@atu.ie>
2023-04-19 17:52:05 +0100

    update the comments

--- account.py
+++ account.py
@@ -1,4 +1,4 @@
-# Reads in a 10 character account number and outputs the account number
+# Reads in an account number(with any length)and outputs the account number
 # with only the last 4 digits showing (and the first 6 digits replaced with Xs)
 # Author: Jaime Lara Carrillo
 
