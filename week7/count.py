ac53bc74e5d8480373a7777341e27e42e0636425
Jaime Lara Carrillo <G00425663@atu.ie>
2023-04-20 13:43:15 +0100

    upper and lowercase e modified

--- week7/count.py
+++ week7/count.py
@@ -1,4 +1,4 @@
-# Program that reads in a text file and outputs the number of e's it contains
+# Program that reads in a text file and outputs the number of e's it contains,including both upper and lowercase
 # Author : Jaime Lara Carrillo
 
 import sys
@@ -6,6 +6,6 @@ import sys
 filename = sys.argv[1]
 with open(filename) as file:
     text = file.read()
-count = text.count("e")
+count = text.lower().count("e")
 
-print("The file", filename, "has", count, "letter'e'.")
+print("The file", filename, "has", count, "letter 'e', .") 
