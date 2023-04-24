b6799950eb93b8d58b0967dd30587b7c0188316b
Jaime Lara Carrillo <G00425663@atu.ie>
2023-04-24 14:57:02 +0100

    week08

---
+++ plottask.py
@@ -0,0 +1,20 @@
+# This program displays:
+# An histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2 
+# A plot of the function  h(x)=x^3 in the range [0, 10]
+# Author: Jaime Lara Carrillo
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+norm_data = np.random.normal(loc=5, scale=2, size=1000)
+x = np.linspace(0, 10)
+y = x ** 3
+
+plt.plot(x, y, linestyle="dashed", color="orange", label="h(x) = x^3")    # Customise the format
+plt.hist(norm_data, label="Normal_distribution")
+
+plt.title("Plottask")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.legend(loc="upper left")    # This line adds a legend to the plot in the upper left corner
+plt.show()
